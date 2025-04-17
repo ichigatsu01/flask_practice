69b14c75ba3bc35c2fb6d63f763e347b290fd055
ichigatsu <firstmoon1986@gmail.com>
2025-04-18 06:23:29 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+# ..\flask_test\Scripts\activate 開始
+# deactivate 終了
+# http://localhost:8000/
+
+
+from flask import Flask, render_template, request
+
+app = Flask("helloapp")
+# msg_lists = []
+msg_lists = []
+handle_rest = False
+
+def rendering(msg):
+    return render_template(
+        'index.html',
+        title = 'Hello',
+        msg = msg,
+        msg_lists = msg_lists
+    )
+
+# def render_main_page(msg="", title="メッセージアプリ"):
+#     return render_template("index.html", title=title, msg=msg, msg_lists=msg_lists)
+
+
+#methodsでGETとPOSTを指定
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST' :
+        global msg_lists
+        action = request.form['action']
+        msg = request.form['msg']
+        
+        if action == "送信":
+            msg_lists.append(msg)
+            msg = 'You typed ' + msg
+            # return render_template(
+            #     'index.html',
+            #     title = 'Hello',
+            #     msg_lists = msg_lists
+            # )
+            return rendering(msg)
+        else:
+            msg_lists = []
+            msg = 'テキストをクリアしました'
+            # return render_template(
+            #     'index.html',
+            #     title = 'Hello',
+            #     msg_lists = msg_lists
+            # )
+            return rendering(msg)
+    else:
+        msg = '入力した文字が追加されていきます。'
+        # return render_template(
+        #     'index.html',
+        #     title = 'Hello',
+        # )
+        return rendering(msg)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1')
